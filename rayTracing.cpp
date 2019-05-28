df65836a530c6d927c948797bb67c8596268733c
kelailei <kelailei@51hitech.com>
2019-05-28 09:29:36 +0800

    Chapter7:Diffuse Materials

--- rayTracing.cpp
+++ rayTracing.cpp
@@ -9,7 +9,22 @@
 #define ImageWidth  200;
 #define ImageLength  100;
 #define random(a, b)   (rand()%(b - a +1 ) +a )
+#define randomNum	(float((rand()%100)) /100.0f)
+//#define randomNum (float((rand()%(100)))/100.0f)
 using namespace std;
+
+
+
+
+vec3 randomPointInUintsphere()
+{
+	vec3  point;
+	do
+	{
+		point = 2.0 * vec3(randomNum, randomNum, randomNum) - vec3(1, 1, 1);
+	} while ( dot(point, point) >= 1.0);
+	return point;
+}
 
 
 vec3 color(const Ray& r, Hitable* world)
@@ -18,7 +33,9 @@ vec3 color(const Ray& r, Hitable* world)
 
 	if (world->hit(r, 0, FLT_MAX, rec ))
 	{
-		return vec3( rec.normal.x() +1 , rec.normal.y()+1, rec.normal.z()+1 )*0.5;
+		vec3   diffuseDir = rec.normal + randomPointInUintsphere();
+
+		return 0.5 * color(Ray(rec.p, diffuseDir), world);
 	}
 	else
 	{
@@ -41,7 +58,7 @@ int main()
 	}
 	int nx = ImageWidth;
 	int ny = ImageLength;
-	int ns = 100;
+	int ns = 10;
 
 	outImage << "P3\n" << nx << " " << ny << "\n255\n";
 	camera    renderCamera;
@@ -68,13 +85,10 @@ int main()
 
 			for (int k = 0; k < ns; k++)
 			{
-				//float u = (float(i) + random(0, 100) / 100) / nx;
-				//float v = (float(j) + random(0, 100) / 100) / ny;
+
 				float u = (float(i) + float(random(0, 100)) / 100.0f) / float(nx);
 				float v = (float(j) + float(random(0, 100)) / 100.0f) / float(ny);
 
-				//float u = float(i)/ float(nx);
-				//float v = float(j)  / float(ny);
 				Ray  r = renderCamera.getRay(u, v);
 
 				pixelColor += color(r, world);
